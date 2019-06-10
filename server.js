05d64ba86ec33f079653b893855f266cf26c2738
Sapna Chauhan <sapna.chauhan@emc.com>
2019-06-09 23:10:22 -0700

    Basic socket communication

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ var PORT = process.env.PORT || 3000;
 var express =require('express');// do an npm install for express in  node command prompt
 var app = express();
 var http = require('http').Server(app);//tells node  to use express app to create an http server
-app.use(express.static(__dirname+'/public'));
-	http.listen(PORT, function()
-	{
-		console.log('Server started');
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname+'/public')); //serving static files in express app e.g app.use('/static', express.static(path.join(__dirname, 'public')))
+
+io.on('connection', function(socket){
+console.log('User connected via Socket.io!');
+	socket.on('disconnect',function(){
+	console.log('User disconnected');
 	});
+});
+
+http.listen(PORT, function()
+{
+	console.log('listening to *:'+PORT);
+});
